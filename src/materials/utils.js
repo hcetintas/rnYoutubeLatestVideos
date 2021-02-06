124889c17b33d30bb8f0ed52bbe4ef360b63fe31
Hayrettin Çetintaş <hayrettincetintas@gmail.com>
2021-02-07 00:07:24 +0300

    videos sayfasını bitirir. react native maps ekler.

---
+++ src/materials/utils.js
@@ -0,0 +1,81 @@
+/* eslint-disable prettier/prettier */
+export const nFormatter = (num, digits) => {
+    var si = [
+        { value: 1, symbol: '' },
+        { value: 1E3, symbol: 'k' },
+        { value: 1E6, symbol: 'M' },
+        { value: 1E9, symbol: 'G' },
+        { value: 1E12, symbol: 'T' },
+        { value: 1E15, symbol: 'P' },
+        { value: 1E18, symbol: 'E' },
+    ];
+    var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    var i;
+    for (i = si.length - 1; i > 0; i--) {
+        if (num >= si[i].value) {
+            break;
+        }
+    }
+    return (num / si[i].value).toFixed(digits).replace(rx, '$1') + si[i].symbol;
+};
+
+export const timeSince = (date) => {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = seconds / 31536000;
+
+    if (interval > 1) {
+        return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+        return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+        return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+        return Math.floor(interval) + " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+        return Math.floor(interval) + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+};
+
+export const convertTime = (duration) => {
+    var a = duration.match(/\d+/g);
+
+    if (duration.indexOf('M') >= 0 && duration.indexOf('H') == -1 && duration.indexOf('S') == -1) {
+        a = [0, a[0], 0];
+    }
+
+    if (duration.indexOf('H') >= 0 && duration.indexOf('M') == -1) {
+        a = [a[0], 0, a[1]];
+    }
+    if (duration.indexOf('H') >= 0 && duration.indexOf('M') == -1 && duration.indexOf('S') == -1) {
+        a = [a[0], 0, 0];
+    }
+
+    duration = 0;
+
+    if (a.length === 3) {
+        duration = duration + parseInt(a[0]) * 3600;
+        duration = duration + parseInt(a[1]) * 60;
+        duration = duration + parseInt(a[2]);
+    }
+
+    if (a.length === 2) {
+        duration = duration + parseInt(a[0]) * 60;
+        duration = duration + parseInt(a[1]);
+    }
+
+    if (a.length === 1) {
+        duration = duration + parseInt(a[0]);
+    }
+    return duration;
+}
--- src/views/videos.js
+++ src/views/videos.js
@@ -1,11 +1,121 @@
 /* eslint-disable prettier/prettier */
+import axios from 'axios';
+import moment from 'moment';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { ActivityIndicator, Image, Dimensions, Text, ImageBackground } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import Box from '../components/box';
+import Colors from '../materials/colors';
+import { nFormatter, timeSince } from '../materials/utils';
+var qs = require('qs');
+import { BASE_URL, GOOGLE_KEY } from '@env';
+const deviceWidth = Dimensions.get('window').width;
+
+
 function VideosScreen({ navigation }) {
+    const [data, setData] = useState({});
+    const [videos, setVideos] = useState([]);
+    const [nextPageToken, setNextPageToken] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    useEffect(() => {
+        getdata();
+        return () => {
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [nextPageToken]);
+    const getdata = async () => {
+        axios.get(BASE_URL, {
+            params: {
+                'part': ['snippet', 'statistics', 'contentDetails'],
+                'chart': 'mostPopular',
+                'maxResults': 10,
+                'regionCode': 'TR',
+                'key': GOOGLE_KEY,
+                'pageToken': nextPageToken,
+            },
+            paramsSerializer: function (params) {
+                return qs.stringify(params, { arrayFormat: 'repeat' })
+            },
+        })
+            .then(function (response) {
+                setData(response.data);
+                setVideos([...videos, ...response.data.items]);
+                setIsLoading(false);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
+    const getMore = () => {
+        if (nextPageToken !== data.nextPageToken &&
+            data.pageInfo.totalResults > videos.length) {
+            setNextPageToken(data.nextPageToken);
+            setIsLoading(true);
+        }
+    };
+
+    const renderItem = ({ item, index }) => {
+        const th = getThumbnailsURL(item.snippet.thumbnails);
+        return (<Box width={deviceWidth} key={index}>
+            <Box elevation={3} borderRadius={6} borderColor={Colors.gray} mt={4} mb={4}>
+                <Image
+                    style={{ width: deviceWidth, height: deviceWidth * (parseFloat(th.height / th.width)) }}
+                    source={{ uri: th.url }} />
+                <Box flexDirection="row" m={8}>
+                    <Box as={Image}
+                        size={36}
+                        borderRadius="full"
+                        source={{ uri: th.url }} />
+                    <Box flex={1} ml={8} flexDirection="column">
+                        <Text numberOfLines={2}>{item.snippet.title}</Text>
+                        <Box flexDirection="row"  >
+                            <Box as={Text} color={Colors.gray}>{index + '-' + item.snippet.channelTitle}</Box>
+                            <Box
+                                mt={8}
+                                ml={4}
+                                mr={4}
+                                size={4}
+                                bg={Colors.gray}
+                                borderRadius="full"
+                            />
+                            <Box as={Text} color={Colors.gray}>{nFormatter(item.statistics.viewCount, 1)}</Box>
+                            <Box
+                                mt={8}
+                                ml={4}
+                                mr={4}
+                                size={4}
+                                bg={Colors.gray}
+                                borderRadius="full"
+                            />
+                            <Box as={Text} color={Colors.gray}>{`${timeSince(new Date(item.snippet.publishedAt))} ago`}</Box>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>);
+    };
+    const footer = () => {
+        return (
+            <Box flex={1} alignItems="center">
+                {(isLoading === true) && <ActivityIndicator size="large" color={Colors.gray} />}
+            </Box>
+        );
+    };
+    const getThumbnailsURL = (thumbnails) => {
+        return thumbnails.maxres || thumbnails.high || thumbnails.standart || thumbnails.medium || thumbnails.default;
+    };
+
     return (
-        <Box flex={1} justifyContent="center" alignItems="center" >
-            <Text>Coming Soon...</Text>
+        <Box flex={1} justifyContent="center" >
+            <FlatList
+                data={videos}
+                renderItem={renderItem}
+                keyExtractor={(item, index) => index.toString()}
+                onEndReached={getMore}
+                onEndReachedThreshold={0.1}
+                ListFooterComponent={footer}
+            />
         </Box >
     );
 }
